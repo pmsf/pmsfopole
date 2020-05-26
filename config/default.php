b2dda6cafb616238faa580634cdfc7041ccd7f29
whitewillem <jezuswateengedoe@gmail.com>
2020-05-27 01:43:33 +0200

    fix default.php

--- config/default.php
+++ config/default.php
@@ -20,14 +20,57 @@ foreach ($libs as $file) {
 setSessionCsrfToken();
 
 
-$enableCsrf = true;                                                 // Don't disable this unless you know why you need to :)
-$sessionLifetime = 43200;                                           // Session lifetime, in seconds
-$enableDebug = false;
+/* General Settings */
+$enableCsrf = true; // Don't disable this unless you know why you need to :)
+$sessionLifetime = 43200;
 
-$locale = 'en';  // Supports 'en', 'de' & 'pl'
+$locale = 'en';
 
-$pageName = 'PMSFopole';
-$headerName = 'PMSFopole';
-$headerImage = 'static/images/headerimage.png';
+$title = 'pmsfopole';
+$favIcon = 'custom/headerimage.png';
 
-$scanner = 'rdm';
+$headerName = 'pmsfopole';
+$headerImage = 'custom/headerimage.png';
+
+$footer = true;
+
+
+/* Urls */
+$homeUrl = '#';
+$mapUrl = '#';
+
+$patreonUrl = '#';
+$paypalUrl = '#';
+
+$discordUrl = '#';
+$telegramUrl = '#';
+
+
+/* Icons */
+$pokemonIconPath = 'custom/sprites/PkmnShuffleMap/PMSF_icons_large/';
+$itemIconPath = 'custom/sprites/pogoassets/no_border/rewards/';
+$eggIconPath = 'custom/sprites/PkmnShuffleMap/PMSF_icons_large/';
+
+
+/* Discord Settings */
+$discord_login = false;
+$forced_discord_auth = false;
+$discord_bot_client_id = 000000000000000000000;
+$discord_bot_client_secret = "secret";
+$discord_bot_redirect_uri = "https://website.com/discord-callback.php";
+$discord_guild_ids = [000000000000000];
+$discord_bot_token = "token";
+
+
+/* Pages */
+$pokemonPage = true;
+$raidPage = true;
+$rewardPage = true;
+$invasionPage = true;
+$shinyPage = false;   // Does not work for mad yet.
+
+
+/* Database Settings */
+$backend = 'rdm';     // rdm or mad
+$queryDelay = 5;      // get data every x seconds.
+$enableDebug = false; // for debugging raw data.
