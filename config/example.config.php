19998ba54dc024fafa7262464012d70635f5df98
whitewillem <jezuswateengedoe@gmail.com>
2020-06-12 14:48:07 +0200

    support for shiny page on mad backend

--- config/example.config.php
+++ config/example.config.php
@@ -56,7 +56,7 @@ $pokemonPage = true;
 $raidPage = true;
 $rewardPage = true;
 $invasionPage = true;
-$shinyPage = false;   // Does not work for mad yet.
+$shinyPage = true;
 
 /* Nest Page */
 $nestPage = true;
